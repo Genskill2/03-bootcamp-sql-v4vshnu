593837272103a03f54627395bdd9fbff48f4c961
v4vshnu <me.vishnu24399@gmail.com>
2021-06-19 22:34:13 +0530

    Fixed query3

--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-SELECT title FROM books WHERE id IN (SELECT book FROM books_subjects WHERE subject IN (SELECT id FROM subjects WHERE name = 'POLITICS' OR name = 'TECHNOLOGY'));
+SELECT title FROM books WHERE id IN (SELECT book FROM books_subjects WHERE subject IN (SELECT id FROM subjects WHERE name = 'Politics' OR name = 'Technology'));
