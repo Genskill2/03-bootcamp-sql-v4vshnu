a62fc07a94c8651020dc552c6afca4e8601f5aa1
v4vshnu <me.vishnu24399@gmail.com>
2021-06-19 22:10:03 +0530

    Fixed delete

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-DELETE FROM books_subjects WHERE subject IN (SELECT id FROM subject WHERE name = "History");
+DELETE FROM books_subjects WHERE subject IN (SELECT id FROM subjects WHERE name = "History");
 DELETE FROM subjects WHERE name = "History";
